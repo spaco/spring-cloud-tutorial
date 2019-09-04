2090750310b4c210b9752c782db597a74bf36d62
chentianzhu <chentianzhu@zeaho.com>
2019-09-04 11:54:19 +0800

    Add stream

---
+++ stream-input/src/main/java/com/spaco/streamoutput/StreamOutputApplication.java
@@ -0,0 +1,13 @@
+package com.spaco.streamoutput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamOutputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamOutputApplication.class, args);
+    }
+
+}
