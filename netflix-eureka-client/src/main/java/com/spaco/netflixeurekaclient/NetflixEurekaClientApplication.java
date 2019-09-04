03a20ed2e3c55a7e117ae9e8b48fa5d0a9a8bb0c
chentianzhu <chentianzhu@zeaho.com>
2019-09-04 11:43:04 +0800

    Add stream

---
+++ netflix-eureka-client/src/main/java/com/spaco/netflixeurekaclient/NetflixEurekaClientApplication.java
@@ -0,0 +1,15 @@
+package com.spaco.netflixeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NetflixEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixEurekaClientApplication.class, args);
+    }
+
+}
---
+++ stream-input/src/main/java/com/spaco/streaminput/StreamInputApplication.java
@@ -0,0 +1,13 @@
+package com.spaco.streaminput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamInputApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamInputApplication.class, args);
+    }
+
+}
