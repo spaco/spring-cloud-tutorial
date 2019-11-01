730e1a55e9284488c3432dd69d8b7581cdd45c1d
chentianzhu <chentianzhu@zeaho.com>
2019-11-01 18:49:30 +0800

    Add task

---
+++ task/src/test/java/com/spaco/task/TaskApplicationTests.java
@@ -0,0 +1,11 @@
+package com.spaco.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
